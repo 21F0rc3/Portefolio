210802309e36b141ae6a47b6236c6346527dc3bb
Gabriel Fernandes <force2606@gmail.com>
2022-01-19 00:18:27 +0000

    Email[C

--- routes.py
+++ routes.py
@@ -1,8 +1,36 @@
 from flask import Flask
 from flask import render_template
+from flask_mail import Mail
+from flask_mail import Message
+from flask import request
+
+import os
 
 app = Flask(__name__)
+
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": os.environ['EMAIL_USER'],
+    "MAIL_PASSWORD": os.environ['EMAIL_PASSWORD']
+}
+
+app.config.update(mail_settings)
+mail = Mail(app)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/sendEmail", methods=['POST'])
+def sendEmail():
+    msg = Message(request.form['Assunto'],
+                  body=request.form['Content'],
+                  sender=request.form['Email'],
+                  recipients=[os.environ['EMAIL_USER']]);
+
+    mail.send(msg);
+    return index();
