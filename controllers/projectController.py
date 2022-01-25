363aac34f90dcfa61eaa31584e08afca06343ec6
Gabriel Fernandes <force2606@gmail.com>
2022-01-25 10:25:10 +0000

    Refactoring projectController

--- controllers/projectController.py
+++ controllers/projectController.py
@@ -3,23 +3,27 @@ from models.project import project
 from models.section import section
 from flask import render_template
 
-
-# Vai buscar o conteudo do projeto a base de dados com um nome especifico
+"""
+    Retorna a pagina do projeto com seu conteudo populado dinamicamente
+"""
 def getProjectContent(project_name):
     project_id = project.query.filter_by(name=project_name).first().id
 
-    # Vai buscar todas as sections do projeto
-    project_sections = section.query.filter_by(project_id=project_id).all() 
+    project_sections_url = section.query.filter_by(project_id=project_id).all() 
 
     # Substitui todos os ficheiros da tabela da secção pelo o seu respetivo conteudo
-    sections = replaceFilesURLForContent(project_sections)
+    project_sections = replaceFilesURLForContent(project_sections_url)
 
-    return render_template("/layouts/project.html", title=project_name, sections=sections)
+    return render_template("/layouts/project.html", title=project_name, sections=project_sections)
 
-# Vai buscar o conteudo de um ficheiro
-def getFileContent(file_url):
+
+
+"""
+    Le um ficheiro
+"""
+def readFile(file_url):
     try:
-        file = open('files/'+file_url, 'r', encoding='utf-8').read()
+        file = open('files/'+file_url, 'r', encoding='utf-8').readlines()
     
     except:
         print("Algo deu errado ao tentar acessar o ficheiro: "+file_url)
@@ -27,7 +31,11 @@ def getFileContent(file_url):
 
     return file
 
-# Substitui o content_file(url para ficheiro) pelo o seu conteudo
+
+
+"""
+    Substitui o content_file(url para ficheiro) pelo o seu conteudo
+"""
 def replaceFilesURLForContent(project_sections):
     sections = []
 
@@ -38,7 +46,7 @@ def replaceFilesURLForContent(project_sections):
         aux.title = project_section.title
         aux.image_url = project_section.image_url
         
-        aux.content_file = getFileContent(project_section.content_file)
+        aux.content_file = readFile(project_section.content_file)
 
         sections.append(aux)
 
