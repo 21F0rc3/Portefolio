a0b797f0df058bd50d67dd9b9afa3fae2dd8770d
Gabriel Fernandes <force2606@gmail.com>
2022-01-29 21:28:24 +0000

    New lang dropdown layout

--- static/js/lang.js
+++ static/js/lang.js
@@ -1,25 +1,44 @@
 function googleTranslateElementInit() {
-    new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'en,pt', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, autoDisplay: false}, 'google_translate_element');
+    new google.translate.TranslateElement({/*pageLanguage: 'en',*/includedLanguages: 'en,pt', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, autoDisplay: false}, 'google_translate_element');
   }
   
- 
+var selectedLang = 'en';
 
-  function translateLanguage(lang) {
+$(".dropdown-menu").find("a").click(function() {   
+  var newElem = "<div class='line lineAnimation'><div>"+$(this).html()+"</div></div>";
+
+  $(".line").remove();
+
+  $("#line-container").append(newElem);
+  
+  setTimeout(function() {
+    $(newElem).remove();
+  }, 6250);
+ });
+
+function translateLanguage(lang) {
       var $frame = $('.goog-te-menu-frame:first');
       if (!$frame.size()) {
           alert("Error: Could not find Google translate frame.");
           return false;
       }
-      $frame.contents().find('.goog-te-menu2-item span.text:contains(' + lang + ')').get(0).click();
-  
-      $("a").click(function() {   
-        $("#line").addClass("lineAnimation");
-        $("#line").find("div").html($(this).html());
-    
-        setTimeout(function() {
-          $("#line").removeClass("lineAnimation");
-        }, 6250);
-       });
+
+      switch(lang) {
+        case 'en':
+          if(selectedLang != 'en') {
+            $frame.contents().find('.goog-te-menu2-item').get(0).click();
+            break;
+          }
+          break;
+        case 'pt':
+          if(selectedLang != 'pt') {
+            $frame.contents().find('.goog-te-menu2-item').get(0).click();
+            break;
+          }
+          break;
+      }
+
+      selectedLang = lang;
 
       return false;
   }  
