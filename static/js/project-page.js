4adcdd06b949a9fad7a39200b01e64a541c26289
Gabriel Fernandes <force2606@gmail.com>
2022-01-30 00:05:25 +0000

    Fixed navbar in projects pages

---
+++ static/js/project-page.js
@@ -0,0 +1,13 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("projectNavbar").style.top = "0";
+  }else {
+    document.getElementById("projectNavbar").style.top = "-50px";
+  }
+  
+  prevScrollpos = currentScrollPos;
+}
